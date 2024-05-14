a184ea0d1e764900d43c643005b1570795f7006e
moonjayoung <jayounge99@gmail.com>
2024-05-14 17:59:00 +0900

    update

--- front/front_pjt/src/main.js
+++ front/front_pjt/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
+import { useKakao } from 'vue3-kakao-maps/@utils'
 import router from './router'
 
+useKakao('021b90905734d34c992a1ca610cde403')
 const app = createApp(App)
 
 app.use(createPinia())
--- front/front_pjt/src/views/HomeView.vue
+++ front/front_pjt/src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
         <h1>HomePage</h1>
+        <!-- <Map /> -->
+        <kakaoMap />
     </div>
 </template>
 
 <script setup>
-
+import Map from '@/components/Map.vue'
+import kakaoMap from '@/components/kakaoMap.vue'
 </script>
 
 <style scoped>
