6f6e1d0f0015d5054421709b3caddfdcb24dc873
yyurikim <yuurrriiii@naver.com>
2024-05-16 16:17:03 +0900

    Y: 환율 계산기 vuetify 적용

--- front/front_pjt/components/ExrateCalculator.vue
+++ front/front_pjt/components/ExrateCalculator.vue
@@ -1,36 +1,72 @@
 <template>
-  <div>
-    <h2>환율 계산기</h2>
-    <p>기준일자 : {{ search_date }}</p>
-    <div>
-      <label for="from-currency">From:</label>
-      <select id="from-currency" v-model="fromCurrency">
-        <option disabled value="">Select Currency</option>
-        <option v-for="rate in rates" :key="rate.id" :value="rate.cur_unit">
-          {{ rate.cur_nm }}
-        </option>
-      </select>
-      <input type="number" v-model.number="amountFrom" placeholder="Enter amount">
+    <v-app>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <h2>환율 계산기</h2>
+            <p>기준일자: {{ search_date }}</p>
+          </v-col>
+        </v-row>
+      <v-row align="center">
+        <v-col cols="12" md="6">
+          <v-combobox
+            id="from-currency"
+            label="From"
+            v-model="fromCurrency"
+            :items="currencies"
+            item-text="cur_nm"
+            item-value="cur_unit"
+            outlined
+          ></v-combobox>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+            v-model.number="amountFrom"
+            label="금액 입력"
+            type="number"
+            placeholder="Enter amount"
+            outlined
+            dense
+          ></v-text-field>
+        </v-col>
+      </v-row>
+        <v-col>
+            <v-btn color="primary" @click="convertCurrency" :disabled="!fromCurrency || !toCurrency">Convert</v-btn>
+          </v-col>
+      <!-- <input type="number" v-model.number="amountFrom" placeholder="Enter amount">
       <button @click="convertCurrency">Convert</button>
-    </div>
-    <div>
-      <label for="to-currency">To:</label>
-      <select id="to-currency" v-model="toCurrency">
-        <option disabled value="">Select Currency</option>
-        <option v-for="rate in rates" :key="rate.id" :value="rate.cur_unit">
-          {{ rate.cur_nm }}
-        </option>
-      </select>
-      <span v-if="displayExchangeAmount">
-        {{ displayExchangeAmount }}
-      </span>
-    </div>
-    <div v-if="isDateMismatch">
-      <p>가장 최근 영업일의 환율 정보입니다.</p>
-    </div>
-  </div>
-    
-
+    </div> -->
+    <v-row align="center">
+        <v-col cols="12" md="6">
+          <v-combobox
+            id="from-currency"
+            label="To"
+            v-model="toCurrency"
+            :items="currencies"
+            item-text="cur_nm"
+            item-value="cur_unit"
+            outlined
+          ></v-combobox>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-text-field
+            v-model="displayExchangeAmount"
+            label="결과"
+            readonly
+            outlined
+            dense
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row v-if="isDateMismatch">
+        <v-col>
+          <v-alert type="warning" dense>
+            가장 최근 영업일의 환율 정보입니다.
+          </v-alert>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script setup>
@@ -45,12 +81,14 @@ const toCurrency = ref('');
 const amountFrom = ref(0);
 const exchangeAmount = ref(null);
 const isDateMismatch = ref(false);
+const currencies = ref([]);
 
 const fetchRates = async () => {
   try {
     const response = await axios.get('http://localhost:8000/exrates/');
     rates.value = response.data.exchange_rates;
     search_date.value = response.data.search_date;
+    currencies.value = response.data.exchange_rates.map(rate => rate.cur_nm);
     console.log(response.data);
   } catch (error) {
     console.error("There was an error!", error);
@@ -58,15 +96,15 @@ const fetchRates = async () => {
 };
 
 watch(fromCurrency, (newValue) => {
-  if (newValue !== 'KRW') {
-    toCurrency.value = 'KRW';
+  if (newValue !== '한국 원') {
+    toCurrency.value = '한국 원';
   }
 });
 
 // toCurrency가 변경될 때 fromCurrency를 KRW로 설정
 watch(toCurrency, (newValue) => {
-  if (newValue !== 'KRW') {
-    fromCurrency.value = 'KRW';
+  if (newValue !== '한국 원') {
+    fromCurrency.value = '한국 원';
   }
 });
 
@@ -78,15 +116,15 @@ const convertCurrency = () => {
     isDateMismatch.value = false;
   }
 
-  const rateFrom = rates.value.find(rate => rate.cur_unit === fromCurrency.value)?.deal_bas_r || 1;
-  const rateTo = rates.value.find(rate => rate.cur_unit === toCurrency.value)?.deal_bas_r || 1;
+  const rateFrom = rates.value.find(rate => rate.cur_nm === fromCurrency.value)?.deal_bas_r || 1;
+  const rateTo = rates.value.find(rate => rate.cur_nm === toCurrency.value)?.deal_bas_r || 1;
   if (rateFrom && rateTo && amountFrom.value) {
     exchangeAmount.value = (amountFrom.value / rateTo) * rateFrom;
 
-    if (fromCurrency.value === 'JPY(100)' || fromCurrency.value === 'IDR(100)') {
+    if (fromCurrency.value === '일본 옌' || fromCurrency.value === '인도네시아 루피아') {
       exchangeAmount.value /= 100;
     }
-    if (toCurrency.value === 'JPY(100)' || toCurrency.value === 'IDR(100)') {
+    if (toCurrency.value === '일본 옌' || toCurrency.value === '인도네시아 루피아') {
       exchangeAmount.value *= 100;
     }
   } else {
